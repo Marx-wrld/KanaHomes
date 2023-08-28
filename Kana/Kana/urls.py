7b65063ba5b559c5505a91237055f13a5e6cfa1d
Marx-wrld <iongonga@kabarak.ac.ke>
2023-08-28 18:04:23 +0300

    product details page

--- Kana/Kana/urls.py
+++ Kana/Kana/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 
 from core.views import frontpage, shop
+from product.views import product
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('shop/', shop, name='shop'),
+    path('product/', product, name='product'),
     path('admin/', admin.site.urls),
 ]
