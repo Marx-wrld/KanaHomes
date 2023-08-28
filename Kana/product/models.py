866efae7a57be755d90a1f2a42c6dc30c1687e57
Marx-wrld <iongonga@kabarak.ac.ke>
2023-08-28 12:41:16 +0300

    db model for our category

--- Kana/product/models.py
+++ Kana/product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Creating a db model for our categories
+
+class category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta: 
+        ordering = ('name',)
+    
+    def __str__(self):
+        return self.name
