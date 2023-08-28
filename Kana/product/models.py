c04de083baa1e84fea066864fcfb3095c00e56b8
Marx-wrld <iongonga@kabarak.ac.ke>
2023-08-28 13:09:25 +0300

    Products db model

--- Kana/product/models.py
+++ Kana/product/models.py
@@ -11,3 +11,21 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+    
+#creating a db model for our products
+class Product(models.Model):
+    #foreign key
+    #CASCADE so that if we delete the category we delete all of its products
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    #setting the ordering so that the newest product comes first
+    class Meta: 
+        ordering = ('-created_at',)
+    
+    def __str__(self):
+        return self.name
