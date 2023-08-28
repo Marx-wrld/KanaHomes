f44b805ba0a9cefe18e13cbb782e89d2d0a3c629
Marx-wrld <iongonga@kabarak.ac.ke>
2023-08-28 17:27:45 +0300

    showing active directory

--- Kana/core/views.py
+++ Kana/core/views.py
@@ -10,8 +10,14 @@ def shop(request):
     categories = Category.objects.all()
     products = Product.objects.all()
 
+    active_category = request.GET.get('category', '')
+
+    if active_category:
+        products = products.filter(category__slug=active_category)
+
     context = {
         'categories': categories,
-        'products': products
+        'products': products,
+        'active_category': active_category
     }
     return render(request, 'core/shop.html', context)
