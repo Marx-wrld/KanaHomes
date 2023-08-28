d74c43b03849d856dd5ba88edbf4f9bf7f1b9fc6
Marx-wrld <iongonga@kabarak.ac.ke>
2023-08-28 17:30:40 +0300

    Shop page and its categories

--- Kana/Kana/urls.py
+++ Kana/Kana/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import frontpage
+from core.views import frontpage, shop
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('shop/', shop, name='shop'),
     path('admin/', admin.site.urls),
 ]
--- Kana/core/views.py
+++ Kana/core/views.py
@@ -5,3 +5,7 @@ from product.models import Product
 def frontpage(request):
     products = Product.objects.all()[0:8] #limiting our products so that only 8 appear on the frontpage
     return render(request, 'core/frontpage.html', {'products': products}) #appending our products so that it appears on the frontpage
+
+def shop(request):
+    products = Product.objects.all()
+    return render(request, 'core/shop.html', {'products': products})
