2be55f37c0f5af6c33827d04e41949a872866a70
Marx-wrld <iongonga@kabarak.ac.ke>
2023-08-28 17:37:03 +0300

    showing product details page in reusable format

--- Kana/Kana/urls.py
+++ Kana/Kana/urls.py
@@ -7,6 +7,6 @@ from product.views import product
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('shop/', shop, name='shop'),
-    path('product/', product, name='product'),
+    path('shop/<slug:slug>/', product, name='product'), #This means that we want slug to be a dynamic part of the url, so here we expect a slug and we give it a name of slug
     path('admin/', admin.site.urls),
 ]
