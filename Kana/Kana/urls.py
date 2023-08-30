eeb695af03aeaaa8603f2a20e3aa46a03a01ebf8
Marx-wrld <iongonga@kabarak.ac.ke>
2023-08-30 18:54:32 +0300

    signup html file build

--- Kana/Kana/urls.py
+++ Kana/Kana/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from cart.views import add_to_cart
-from core.views import frontpage, shop
+from core.views import frontpage, shop, signup
 from product.views import product
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('signup/', signup, name='signup'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'), #This means that we want slug to be a dynamic part of the url, so here we expect a slug and we give it a name of slug
     path('add_to_cart/<int:product>/', add_to_cart, name='add_to_cart'),
