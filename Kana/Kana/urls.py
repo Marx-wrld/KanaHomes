cb79ca8155ef51764bc1297a2db6b08a7a1adc4d
Marx-wrld <iongonga@kabarak.ac.ke>
2023-08-30 19:09:55 +0300

    logout function build

--- Kana/Kana/urls.py
+++ Kana/Kana/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
@@ -9,7 +10,8 @@ from product.views import product
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('signup/', signup, name='signup'),
-    # path('login/', login, name='login'),
+    path('login/', login_old, name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'), #This means that we want slug to be a dynamic part of the url, so here we expect a slug and we give it a name of slug
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
