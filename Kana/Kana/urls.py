e633d7595711ccc4fd4a83509218f23464e4bc3b
Marx-wrld <iongonga@kabarak.ac.ke>
2023-08-30 18:17:55 +0300

    Creating view for adding to cart

--- Kana/Kana/urls.py
+++ Kana/Kana/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-
+from cart.views import add_to_cart
 from core.views import frontpage, shop
 from product.views import product
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'), #This means that we want slug to be a dynamic part of the url, so here we expect a slug and we give it a name of slug
+    path('add_to_cart/<int:product>/', add_to_cart, name='add_to_cart'),
     path('admin/', admin.site.urls),
 ]
