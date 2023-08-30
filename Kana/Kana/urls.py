d5b3070d8331e845a5e8ff4754739ab668835437
Marx-wrld <iongonga@kabarak.ac.ke>
2023-08-30 18:58:40 +0300

    Login page

--- Kana/Kana/urls.py
+++ Kana/Kana/urls.py
@@ -7,6 +7,7 @@ from product.views import product
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('signup/', signup, name='signup'),
+    path('login/', login, name='login'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'), #This means that we want slug to be a dynamic part of the url, so here we expect a slug and we give it a name of slug
     path('add_to_cart/<int:product>/', add_to_cart, name='add_to_cart'),
--- Kana/core/views.py
+++ Kana/core/views.py
@@ -10,6 +10,9 @@ def frontpage(request):
 def signup(request):
     return render(request, 'core/signup.html')
 
+def login(request):
+    return render(request, 'core/login.html')
+
 def shop(request):
     categories = Category.objects.all()
     products = Product.objects.all()
