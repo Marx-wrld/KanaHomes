d850e19802d8fbb90040d9de1928099c975e6a29
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-04 10:49:05 +0300

    Allow checkout for only authenticated users

--- Kana/cart/views.py
+++ Kana/cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .cart import Cart
 
 # Create your views here.
@@ -11,5 +12,6 @@ def add_to_cart(request, product_id):
 def cart(request):
     return render(request, 'cart/cart.html')
 
+@login_required
 def checkout(request):
     return render(request, 'cart/checkout.html')
