a5dbded4555ffd46c6b32dc323b0876e61b2044a
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-04 11:15:24 +0300

    view and url for updating cart

--- Kana/cart/views.py
+++ Kana/cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .cart import Cart
+from product.models import Product
 
 # Create your views here.
 def add_to_cart(request, product_id):
@@ -11,6 +12,34 @@ def add_to_cart(request, product_id):
 
 def cart(request):
     return render(request, 'cart/cart.html')
+
+def update_cart(request, product_id, action):
+    cart = Cart(request)
+
+    if action == "increment":
+        cart.add(product_id, 1, True)
+    else:
+        cart.add(product_id, -1, True)
+
+    product = Product.objects.get(pk=product_id)
+    quantity = cart.get_item(product_id)
+
+    item = {
+        'product': {
+            'id': product.id,
+            'name': product.name,
+            'image': product.image,
+            'get_thumbnail': product.get_thumbnail(),
+            'price': product.price,
+        },
+        'total_price': (quantity * product.price) / 100,
+        'quantity': quantity,
+    }
+
+    response = render(request, 'cart/partials/cart_item.html', {'item': item})
+    response['HX-Trigger'] = 'update-menu-cart'
+
+    return response
 
 @login_required
 def checkout(request):
