38b3f4afa92ac2d3496a7810f529fcce604f2462
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-04 11:20:41 +0300

    separating our url files per app

--- Kana/Kana/urls.py
+++ Kana/Kana/urls.py
@@ -1,22 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
-from cart.views import add_to_cart, cart, checkout
-from core.views import frontpage, shop, signup
-from product.views import product
+from django.urls import path, include
 
 urlpatterns = [
-    path('', frontpage, name='frontpage'),
-    path('signup/', signup, name='signup'),
-    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('shop/', shop, name='shop'),
-    path('shop/<slug:slug>/', product, name='product'), #This means that we want slug to be a dynamic part of the url, so here we expect a slug and we give it a name of slug
-    path('cart/', cart, name='cart'),
-    path('cart/checkout', checkout, name='checkout'),
-    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    #We need to tell django where we moved the urls to
+    path('', include('core.urls')),
+    path('cart/', include('cart.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
---
+++ Kana/core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth import views
+from core.views import frontpage, shop, signup
+from product.views import product
+
+
+urlpatterns = [
+    path('', frontpage, name='frontpage'),
+    path('signup/', signup, name='signup'),
+    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('shop/', shop, name='shop'),
+    path('shop/<slug:slug>/', product, name='product'), #This means that we want slug to be a dynamic part of the url, so here we expect a slug and we give it a name of slug 
+]
