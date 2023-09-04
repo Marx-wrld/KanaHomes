84dbe20b031470c411ba932dea1196d2af4d30af
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-04 10:53:13 +0300

    Cart page build

--- Kana/Kana/urls.py
+++ Kana/Kana/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from cart.views import add_to_cart
+from cart.views import add_to_cart, cart
 from core.views import frontpage, shop, signup
 from product.views import product
 
@@ -14,6 +14,7 @@ urlpatterns = [
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'), #This means that we want slug to be a dynamic part of the url, so here we expect a slug and we give it a name of slug
+    path('cart/', cart, name='cart'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
