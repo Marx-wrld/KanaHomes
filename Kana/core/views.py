c5c84099db2cc7cb8e09d936e3f398a26360866f
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-04 10:43:23 +0300

    'Edit account' functionality fix

--- Kana/core/views.py
+++ Kana/core/views.py
@@ -25,12 +25,21 @@ def signup(request):
     return render(request, 'core/signup.html', {'form': form})
 
 @login_required
-def edit_myaccount(request):
-    return render(request, 'core/edit_myaccount.html')
-
-@login_required
 def myaccount(request):
     return render(request, 'core/myaccount.html')
+
+@login_required
+def edit_myaccount(request):
+    if request.method == 'POST':
+        user = request.user
+        user.first_name = request.POST.get('first_name')
+        user.last_name = request.POST.get('last_name')
+        user.username = request.POST.get('username')
+        user.email = request.POST.get('email')
+        user.save()
+
+        return redirect('myaccount')
+    return render(request, 'core/edit_myaccount.html')
 
 def shop(request):
     categories = Category.objects.all()
