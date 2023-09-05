11ded634fde8e94642a2328f921080b26573652c
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-05 17:43:21 +0300

    Decr to zero items fix

--- Kana/cart/views.py
+++ Kana/cart/views.py
@@ -24,17 +24,22 @@ def update_cart(request, product_id, action):
     product = Product.objects.get(pk=product_id)
     quantity = cart.get_item(product_id)
 
-    item = {
-        'product': {
-            'id': product.id,
-            'name': product.name,
-            'image': product.image,
-            'get_thumbnail': product.get_thumbnail(),
-            'price': product.price,
-        },
-        'total_price': (quantity * product.price) / 100,
-        'quantity': quantity,
-    }
+    if quantity:
+        quantity = quantity['quantity']
+
+        item = {
+            'product': {
+                'id': product.id,
+                'name': product.name,
+                'image': product.image,
+                'get_thumbnail': product.get_thumbnail(),
+                'price': product.price,
+            },
+            'total_price': (quantity * product.price) / 100,
+            'quantity': quantity,
+        }
+    else:
+        item = None
 
     response = render(request, 'cart/partials/cart_item.html', {'item': item})
     response['HX-Trigger'] = 'update-menu-cart'
